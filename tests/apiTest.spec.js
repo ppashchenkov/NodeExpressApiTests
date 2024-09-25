819ba68726e5ee0952ec9161d2922f256a273312
ppashchenkov <ppashchenkov@gmail.com>
2024-09-25 22:30:49 +0300

    Version two. Get userId - OK!

--- tests/apiTest.spec.js
+++ tests/apiTest.spec.js
@@ -1,6 +1,6 @@
 import {test, request, expect} from '@playwright/test'
 
-const BASE_URL = 'http://82.142.152.107:5000'
+const BASE_URL = 'http://localhost:5000'
 
 const userFirst = {
     "firstName": "Joe",
@@ -21,17 +21,17 @@ test('GET /', async() => {
     const response = await  apiRequest.get(`${BASE_URL}/`)
 
     const statusCode = response.status()
-    const headersArray = response.headersArray()
-    const contentType = headersArray
-        .find((header) => header.name === 'Content-Type')
-        .value
+    // const headersArray = response.headersArray()
+    // const contentType = headersArray
+    //     .find((header) => header.name === 'Content-Type')
+    //     .value
 
-    console.log(response)
-    console.log("------------------------")
-    console.log(await response.text())
-    console.log(statusCode)
-    console.log(headersArray)
-    console.log("contentType = " + contentType)
+    // console.log(response)
+    // console.log("------------------------")
+    // console.log(await response.text())
+    // console.log(statusCode)
+    // console.log(headersArray)
+    // console.log("contentType = " + contentType)
 
     //Assert response
     expect(await response.text()).toEqual("Node Express API Server")
@@ -55,20 +55,27 @@ test('Create users', async () => {
         data: userFirst
     })
 
+    const responseJSON = await response.json()
+    userId = responseJSON[0].UserID
+
+    console.log("USERID = " + userId)
+    console.log("------------------------")
+    console.log(await response.text())
+
     expect(response.ok()).toBeTruthy();
 })
 
-test('Get users id', async() => {
-    const apiRequest = await request.newContext()
-
-    const response = await apiRequest.get(`${BASE_URL}/users`)
-
-    while (userId.length === 0) {
-        userId = (await response.json())[0].id
-    }
-
-    expect(response.ok()).toBeTruthy()
-})
+// test('Get users id', async() => {
+//     const apiRequest = await request.newContext()
+//
+//     const response = await apiRequest.get(`${BASE_URL}/users`)
+//
+//     // while (userId.length === 0) {
+//     //     userId = (await response.json())[0].id
+//     // }
+//
+//     expect(response.ok()).toBeTruthy()
+// })
 
 // test('PATCH user', async()  => {
 //     const apiRequest = await request.newContext()
@@ -86,21 +93,33 @@ test('Get users id', async() => {
 
 test('GET user by id', async() => {
     const apiRequest = await request.newContext()
-
+    while (userId.length === 0) {
+        const response = await apiRequest.get(`${BASE_URL}/users`)
+        while (userId.length === 0) {
+            userId = (await response.json())[0].id
+        }
+    }
+    console.log("USERID_GET_USER_BY_ID = " + userId)
     const response = await apiRequest.get(`${BASE_URL}/users/${userId}`)
 
-    while (currentFirstName.length === 0) {
-        const responseJson = await response.json()
-        currentFirstName = await responseJson[0].firstName
-    }
+    // const responseJson = await response.json()
+    // currentFirstName = responseJson[0].firstName
 
     expect(response.ok()).toBeTruthy();
-    // expect(currentFirstName).toEqual(userSecond.firstName)
+    // expect(currentFirstName).toEqual(userFirst.firstName)
 })
 
 test('Delete users', async() => {
+    console.log("USERID_DELETE_BEFORE = " + userId)
     const apiRequest = await request.newContext()
 
+    while (userId.length === 0) {
+        const response = await apiRequest.get(`${BASE_URL}/users`)
+        while (userId.length === 0) {
+            userId = (await response.json())[0].id
+        }
+    }
+    console.log("USERID_DELETE = " + userId)
     const response = await apiRequest.delete(`${BASE_URL}/users/${userId}`)
 
     expect(await response.text()).toEqual("User was deleted successfully.")
