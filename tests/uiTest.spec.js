f9176fe691457d8232059ef214a58dfb1bf12018
ppashchenkov <ppashchenkov@gmail.com>
2024-10-20 20:22:36 +0300

    Add uiTest.spec.js

---
+++ tests/uiTest.spec.js
@@ -0,0 +1,17 @@
+import {test, expect} from '@playwright/test'
+
+test('Get Title', async ({page}) => {
+    const p = await page.goto('http://localhost:5000')
+
+    const headerLocator = page.getByRole('heading', { name: 'Node Express API Server App' })
+    const headerCssLocator = page.locator('#appName')
+    
+    await expect(page).toHaveTitle('Users app')
+    await expect(page).toHaveURL('http://localhost:5000/')
+    await expect(headerLocator).toBeVisible()
+    await expect(headerCssLocator).toHaveText('Node Express API Server App')
+})
+
+test('Get Title Promise.all', async ({page}) => {
+    const p = await page.goto('http://localhost:5000')
+})
