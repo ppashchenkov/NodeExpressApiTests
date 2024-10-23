f6fb532fcde1d1039c5f8f40394a7433334bc91f
ppashchenkov <email@example.com>
2024-10-23 15:57:34 +0300

    Promise.allSettled

--- tests/uiTest.spec.js
+++ tests/uiTest.spec.js
@@ -25,3 +25,18 @@ test('Get Title Promise.all', async ({page}) => {
         expect(headerCssLocator).toHaveText('Node Express API Server App'),
     ])
 })
+
+test('Get Title Promise.allSettled', async ({page}) => {
+    const p = await page.goto('http://localhost:5000')
+
+    const headerLocator = page.getByRole('heading', { name: 'Node Express API Server App' })
+    const headerCssLocator = page.locator('#appName')
+
+    const results = await Promise.allSettled([
+        expect(page).toHaveTitle('Users app'),
+        expect(page).toHaveURL('http://localhost:5000/'),
+        expect(headerLocator).toBeVisible(),
+        expect(headerCssLocator).toHaveText('Node Express API Server App'),
+    ])
+    expect(results.filter((result) => result.status === "rejected")).toHaveLength(0)
+})
